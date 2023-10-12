0a5bc63a10b5e2baa982242749493349dc43d470
manishaverma15 <manishaverma15873@gmail.com>
2023-10-13 02:04:26 +0530

    feat: created mock-server to login and register

--- src/mock-server.ts
+++ src/mock-server.ts
@@ -1,30 +1,52 @@
 import { createServer, Model, RestSerializer } from 'miragejs';
 import { v4 as uuidv4 } from 'uuid';
 
+const userToken = '123456789';
+
 function createMockServer() {
- return createServer({
-  models: {
-    user: Model,
-  },
+  return createServer({
+    models: {
+      user: Model,
+    },
 
-  serializers: {
-    application: RestSerializer.extend({
-      embed: true, // Enable embedding data
-      root: false, // Do not include a root element in the response
-    }),
-  },
+    serializers: {
+      application: RestSerializer.extend({
+        embed: true,
+        root: false,
+      }),
+    },
 
-  routes() {
-    this.namespace = 'api';
+    routes() {
+      this.namespace = 'api';
 
+      this.post('/register', (schema: any, request: any) => {
+        const attrs = JSON.parse(request.requestBody);
+        attrs.id = uuidv4();
+        const user = schema.users.create(attrs);
+        console.log('user', user)
+        const token = userToken;
+        console.log('token', token)
+        user.update({ token });
 
-    this.post('/auth/register', (schema: any, request: any) => {
-      const attrs = JSON.parse(request.requestBody);
-      attrs.id = uuidv4();
-      return schema.users.create(attrs);
-    });
-  },
-});
+        return {
+          user: user.attrs,
+          token: token,
+        };
+      });
+
+      this.post('/login', (schema: any, request: any) => {
+        console.log('req', request.requestBody)
+        const { email, password } = JSON.parse(request.requestBody);
+        const user = schema.users.findBy({ email, password });
+        console.log('user', user)
+        if (user) {
+          return { token: userToken };
+        } else {
+          return { error: 'Invalid credentials' };
+        }
+      });
+    },
+  });
 }
 
 export default createMockServer;
