c5b9174fc12597f5b4dd7515d795d495859adbf0
manishaverma15 <manishaverma15873@gmail.com>
2023-10-13 02:04:26 +0530

    feat: created mock-server to login and register

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -8,9 +8,32 @@ export type User = {
   phoneNumber: string;
 }
 
-const BASE_URL = 'api/auth';
+const BASE_URL = 'api';
 
-export async function registerUser(userData: any): Promise<User[]> {
+export async function loginUser(credentials: { email: string; password: string }): Promise<User> {
+  console.log('creds', credentials)
+  try {
+    const response = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    });
+
+    if (!response.ok) {
+      throw new Error('Invalid credentials');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error logging in:', error);
+    throw error;
+  }
+}
+
+export async function registerUser(userData: any): Promise<User> {
   try {
     const response = await fetch(`${BASE_URL}/register`, {
       method: 'POST',
@@ -20,12 +43,13 @@ export async function registerUser(userData: any): Promise<User[]> {
       body: JSON.stringify(userData),
     });
     console.log('user-data', userData)
-    
+
     if (!response.ok) {
       throw new Error('Registration failed');
     }
 
     const data = await response.json();
+    console.log('data', data)
     return data;
 
   } catch (error) {
