1356dce0468ffc1fdbc5a4d0eba7cd8d8b665609
manishaverma15 <manishaverma15873@gmail.com>
2023-10-24 11:23:21 +0530

    fix: UI changes login user form

--- src/components/Form/loginForm/login-form.tsx
+++ src/components/Form/loginForm/login-form.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TextField, Paper, Grid, Button, Box } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
@@ -10,7 +10,18 @@ import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 const validationSchema = yup.object().shape({
-  email: yup.string().email('Invalid email').required('Email is required'),
+  email: yup.string().email('Invalid email').required('Email is required').test(
+    'is-valid-domain',
+    'Invalid email domain',
+    function (value) {
+      if (value) {
+        const validDomains = ['.com', '.org', '.net'];
+        const emailDomain = value.split('.').pop();
+        return validDomains.includes(`.${emailDomain}`);
+      }
+      return true;
+    }
+  ),
   password: yup.string().required('Password is required'),
 });
 
@@ -21,108 +32,93 @@ const LoginForm = () => {
 
   const dispatch = useDispatch<AppDispatch>();
 
-  const { handleSubmit, control, formState: { errors } } = useForm({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm({
     resolver: yupResolver(validationSchema),
   });
 
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleChange = (e: React.FormEvent) => {
-    const target = e.target as HTMLInputElement;
-
-    setFormData({
-      ...formData,
-      [target.name]: target.value,
-    });
+  const onSubmit = (data: any) => {
+    dispatch(userLoggedIn(data));
+    reset();
+    handleNavigate();
   };
 
   const handleNavigate = () => {
     navigate('/dashboard');
   };
 
-  const onSubmit = (e: any) => {
-    e.preventDefault();
-    dispatch(userLoggedIn({ email: formData.email, password: formData.password }));
-    setFormData({
-      email: '',
-      password: '',
-    });
-
-    handleNavigate();
-  };
-
   return (
-    <div className='register-container'>
-      <Paper elevation={3} className='register-paper'>
-        <div>
-          <h4 style={{ textAlign: 'center' }}>To login fill the credentials</h4>
-        </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Controller
-                name="email"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    name='email'
-                    id='outlined-basic'
-                    label='Email'
-                    variant='outlined'
-                    fullWidth
-                    value={formData.email}
-                    onChange={handleChange}
-                    error={!!errors.email}
-                    helperText={errors.email?.message}
-                  />
-                )}
-              />
+    <Box className='main-container'>
+      <Box className='login-container'>
+        <Paper elevation={3} className='login-subContainer'>
+          <Box className='join-us'>
+            <h4 className='title'>Sign in</h4>
+            <span className='title'>Enter your email and password to login</span>
+          </Box>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Controller
+                  name="email"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      name='email'
+                      id='outlined-basic'
+                      label='Email'
+                      variant='outlined'
+                      fullWidth
+                      error={!!errors.email}
+                      helperText={errors.email?.message}
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Controller
+                  name="password"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      name='password'
+                      id='outlined-basic'
+                      label='Password'
+                      variant='outlined'
+                      fullWidth
+                      error={!!errors.password}
+                      helperText={errors.password?.message}
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12} style={{ textAlign: 'center' }}>
+                <Button variant='contained'
+                  color='primary'
+                  style={{ textTransform: 'none' }}
+                  type='submit'
+                >
+                  Login
+                </Button>
+              </Grid>
+              <Grid style={{ textAlign: 'center', margin: 'auto' }}>
+                <Box className='new-account'>
+                  <h6>Do not have an account?</h6>
+                  <Link to='/register'>Register</Link>
+                </Box>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Controller
-                name="password"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    name='password'
-                    id='outlined-basic'
-                    label='Password'
-                    variant='outlined'
-                    fullWidth
-                    value={formData.password}
-                    onChange={handleChange}
-                    error={!!errors.password}
-                    helperText={errors.password?.message}
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item xs={12} style={{ textAlign: 'center' }}>
-              <Button variant='contained'
-                color='primary'
-                style={{ textTransform: 'none' }}
-                type='submit'
-              >
-                Login
-              </Button>
-            </Grid>
-            <Grid style={{ textAlign: 'center', margin: 'auto' }}>
-              <Box className='new-account'>
-                <h6>Do not have an account?</h6>
-                <Link to='/register'>Register</Link>
-              </Box>
-            </Grid>
-          </Grid>
-        </form>
-      </Paper>
-    </div>
+          </form>
+        </Paper>
+      </Box>
+    </Box>
   );
 }
 export default LoginForm;
